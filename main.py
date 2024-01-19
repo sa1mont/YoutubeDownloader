3327d85bf3b85dc76e3fecfcde870d896fec930c
Artemiy <sa1mont@mail.ru>
2024-01-19 05:52:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from pytube import *
 import os
 
-import pytube
+video_url = "https://www.youtube.com/watch?v=VIDEO_ID"
 
-# ������лка на видео YouTube
-video_url = "https://www.youtube.com/watch?v=Sq6tf5xyr4o"
-
-# С����даем объект YouTube
 yt = pytube.YouTube(video_url)
 
-# Получаем все доступные потоки видео
 streams = yt.streams
 
-# Фильтруем потоки по разрешению и формату
 video_stream = streams.filter(res="720p", file_extension="mp4").first()
 
-# Скачиваем видео
 video_stream.download()
 
-# Печатаем сообщение об успешной загрузке
 print("Видео успешно загружено!")
